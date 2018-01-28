a6ef6c130c5d8abcb1fa73946b8f31b287c2fb26
GaborMartin <gabormartin94@gmail.com>
2018-01-28 16:58:36 +0100

    Fixed menu handling, progress saving/loading method

--- Main.java
+++ Main.java
@@ -3,18 +3,14 @@ import java.io.*;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
-    private static Garden garden ;
+    private static Garden garden;
 
     public static void main(String[] args) {
-        System.out.println("Welcome in garden simulation! First of all, please create your profile and garden!\n");
-        Player player = Player.createPlayer();
-        System.out.println("\nYour player's details:");
-        System.out.println(player);
-        garden = Garden.createGarden(player);
-        System.out.println(garden.getCreatures().length);
+        System.out.println("Welcome in garden simulation!\n");
+        handleStart();
         System.out.println("\nYour garden's details:");
         System.out.println(garden + "\n");
-        System.out.println("Available commands: :list, :create, :find, :types, :exit, :save and exit");
+        System.out.println("Available commands: :list, :create, :find, :types, :save progress, :exit");
         while (true) {
             String line = scanner.nextLine();
             if (":exit".equals(line)) {
@@ -33,9 +29,15 @@ public class Main {
                 handleMenu();
 
             } else if (":find".equals(line)) {
-                //handleFind();
-            } else if (":save and exit".equals(line)) {
-                handleSave(garden);
+                handleFindCreature();
+                handleMenu();
+
+            } else if (":save progress".equals(line)) {
+                handleSave();
+                handleMenu();
+            } else if (":load".equals(line)) {
+                handleLoad();
+                handleMenu();
             }
 
             }
@@ -51,18 +53,28 @@ public class Main {
 
     private static void handleList() {
         Creature[] creatures = garden.getCreatures();
+        Tool[] tools = garden.getTools();
 
-        System.out.println("\nThese creatures are in your garden: ");
-        if (garden.getCreatures().length == 0) {
+        System.out.println("\nThese creatures are in your garden:\n");
+        if (creatures.length == 0) {
             System.out.println("\tThere's no any animal or plant in the garden yet!");
         } else {
-            for (int i = 0; i < garden.getCreatures().length; i++) {
-                System.out.println("\t" + garden.getCreatures()[i].getName());
+            for (int i = 0; i < creatures.length; i++) {
+                System.out.println("\t" + creatures[i]);
             }
         }
-    }
+        System.out.println("\nThese tools are in your garden:\n");
+            if (tools.length == 0) {
+                System.out.println("\tThere's no any tool in the garden yet!");
+            } else {
+                for (int i = 0; i < tools.length; i++) {
+                    System.out.println("\t" + tools[i]);
+                }
+            }
+        }
+
     private static void handleCreate() {
-        System.out.println("What is the type of the creature?");
+        System.out.println("What is the type of the creature? (ANIMAL / PLANT)");
         String typeAsString = scanner.nextLine();
         CreatureType type;
         try {
@@ -72,16 +84,29 @@ public class Main {
             return;
         }
         garden.createCreature(type);
+        System.out.println("Creation is done!");
+    }
 
-        System.out.println("Creation is done!ar");
+    private static void handleFindCreature() {
+        Creature[] creatures = garden.getCreatures();
+        System.out.println("Type in the name of the creature you're looking for!");
+        String nameAsString = scanner.nextLine();
+        for (int i = 0; i < creatures.length; i++) {
+            if (creatures[i].getName().equals(nameAsString)) {
+                System.out.println("Creature is found!");
+                System.out.println("\nDetails of the creature:" + "\n" + "Name: " + creatures[i].getName() + ", Type: " + creatures[i].getType() + ", Availability: " + creatures[i].getAvailability());
+            } else {
+                System.out.println("This creature does not exist! Are you sure about the name?");
+            }
+        }
     }
 
     private static void handleMenu() {
         System.out.println("\nWhat would you like to do?");
-        System.out.println("Available commands: :list, :create, :find, :types, :exit, :save and exit");
+        System.out.println("Available commands: :list, :create, :find, :types, :save progress, :exit");
     }
 
-    private static void handleSave(Garden garden) {
+    private static void handleSave() {
         try {
          FileOutputStream fileOut =
          new FileOutputStream("progress.ser");
@@ -93,5 +118,40 @@ public class Main {
       } catch (IOException i) {
          i.printStackTrace();
       }
+    }
+
+    private static void handleLoad() {
+        garden = null;
+      try {
+         FileInputStream fileIn = new FileInputStream("progress.ser");
+         ObjectInputStream in = new ObjectInputStream(fileIn);
+         garden = (Garden) in.readObject();
+         in.close();
+         fileIn.close();
+      } catch (IOException i) {
+         i.printStackTrace();
+         return;
+      } catch (ClassNotFoundException c) {
+         System.out.println("Garden class not found");
+         c.printStackTrace();
+         return;
+      }
+      
+      System.out.println("Your previous progress is loaded...");
+    }
+
+    private static void handleStart() {
+        System.out.println("Would you like to load your previos progress? (y / n)");
+        String answer = scanner.nextLine();
+        if (answer.toLowerCase().equals("y")) {
+            handleLoad();
+        } else if (answer.toLowerCase().equals("n")) {
+            System.out.println("First of all create your player and garden");
+            Player player = Player.createPlayer();
+            System.out.println("\nYour player's details:");
+            System.out.println(player);
+            garden = Garden.createGarden(player);
+            garden.uploadTools("Tools.csv");
+        }
     }
 }
